29a83cd04d04050409aabb81bb23093c18d88059
jbirkenmaier <127735731+jbirkenmaier@users.noreply.github.com>
2024-04-01 12:53:07 +0200

    update

--- function_library.py
+++ function_library.py
@@ -1,11 +1,14 @@
 import tkinter as tk
 import matplotlib.pyplot as plt
 import customtkinter as ctk
+import random
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 def printInput(root,inputtxt): 
     inp = inputtxt.get("end-1c linestart", "end-1c lineend")
     label = tk.Label(root, text = inp)
-    label.pack()
+    #label.pack()
 
 def on_enter(root,event, inputtxt):
     printInput(root,inputtxt)
@@ -18,5 +21,38 @@ def add_activity_button_command(root,event,inputtxt):
     printInput(root,inputtxt)
     inputtxt.delete(1.0, ctk.END)
 
+class Activity:
+    def __init__(self, root, name, typ, row, column):
+        self.root = root
+        self.name = name
+        self.typ = typ
+        self.row = row
+        self.column = column
+        self.plot_graph()
+        
+    def plot_graph(self):
+        #plot_frame = tk.Frame(self.root)
+        #plot_frame.grid(row=self.row, column=self.column)
+
+        x=[i for i in range(4)]
+        y=[random.randint(-10,10) for i in range(4)]
+
+        fig = Figure(figsize=(5, 4), dpi=100)
+        plot = fig.add_subplot(1, 1, 1)
+        plot.plot(x,y, marker='o')
+        plot.set_title('Example Plot')
+        plot.set_xlabel('X-axis')
+        plot.set_ylabel('Y-axis')
+        
+        canvas = FigureCanvasTkAgg(fig, master=self.root)
+        canvas.draw()
+        canvas.get_tk_widget().grid(row=self.row, column=self.column)#grid(row=self.row, column=self.column)
+
+        # Place the canvas on the Tkinter window
+        #canvas.get_tk_widget().place(x=100, y=200)
+
+        #canvas = FigureCanvasTkAgg(fig, master=plot_frame)
+        #canvas.draw()
+        #canvas.get_tk_widget().pack(side="top", fill="both", expand=False)
 
 
--- main.py
+++ main.py
@@ -10,23 +10,56 @@ font_size = 25
 
 def main():
     root = ctk.CTk()
-    font = tk.font.Font(size=font_size)
-    
     #app = BulletJournalApp(root)
-    root.title("Input Window")
+    root.title("Journal-Application")
     root.geometry("1000x500")
 
-    inputtxt = tk.Text(root, height = 1, width = 50, font=font)
-    inputtxt.pack()
-    inputtxt.place(x=100, y=50)
+    font = tk.font.Font(size=font_size)
+    
+    
+    # Create a frame for the left part
+    left_frame = tk.Frame(root,bg="black")
+    left_frame.grid(row=0, column=0, sticky="nsew")
+
+    # Create a frame for the right part
+    right_frame = tk.Frame(root, bg="black")
+    right_frame.grid(row=0, column=1, sticky="nsew")
+
+    # Ensure resizing behavior
+    root.grid_rowconfigure(0, weight=1)
+    root.grid_columnconfigure(0, weight=1)
+    root.grid_columnconfigure(1, weight=1)
+
+    
+    inputtxt = tk.Text(left_frame, height = 1, width = 20, font=font,bg="lightgray", padx=10, pady=5)
+    #inputtxt.pack()
+    #inputtxt.place(x=100, y=50)
+    inputtxt.grid(row=0, column=0)
     inputtxt.bind("<Return>",lambda event: fl.on_enter(root,event, inputtxt))
 
-    add_activity_button = ctk.CTkButton(master=root, text="Add activity")
-    add_activity_button.pack()
-    add_activity_button.place(x=50, y=50)
+    
+
+    add_activity_button = ctk.CTkButton(master=left_frame, text="Add activity")
+    #add_activity_button.pack()
+    #add_activity_button.place(x=50, y=50)
+    add_activity_button.grid(row=0, column=1)
     add_activity_button.bind("<Button-1>",lambda event: fl.add_activity_button_command(root, event, inputtxt))
     #add_activity_button.after_idle(lambda: fl.get_button_width(add_activity_button, inputtxt))  # Get button width after it's fully drawn
-    
+
+
+    activity = fl.Activity(right_frame,"name","typ", 0,0).plot_graph()
+    activity = fl.Activity(right_frame,"name","typ", 0,1).plot_graph()
+    activity = fl.Activity(right_frame,"name","typ", 1,0).plot_graph()
+    activity = fl.Activity(right_frame,"name","typ", 1,1).plot_graph()
+    #activity = fl.Activity(right_frame,"name","typ", 2,0).plot_graph()
+    #activity = fl.Activity(right_frame,"name","typ", 2,1).plot_graph()
+    #activity = fl.Activity(right_frame,"name","typ", 0,2).plot_graph()
+    #activity = fl.Activity(right_frame,"name","typ", 1,2).plot_graph()
+
+    # Configure resizing behavior
+    root.grid_rowconfigure(0, weight=1)
+    root.grid_columnconfigure(0, weight=1)
+        
     root.mainloop()
 
 if __name__ == "__main__":
