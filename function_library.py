db8c532c2496adf257d1a760b0b69ac00d432528
jbirkenmaier <127735731+jbirkenmaier@users.noreply.github.com>
2024-04-01 19:01:06 +0200

    update

--- function_library.py
+++ function_library.py
@@ -5,30 +5,58 @@ import random
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
+activities=[]
+
 def printInput(root,inputtxt): 
     inp = inputtxt.get("end-1c linestart", "end-1c lineend")
     label = tk.Label(root, text = inp)
     #label.pack()
-    
-def on_enter(root,event, inputtxt):
-    printInput(root,inputtxt)
 
+def create_activity():
+    pass
+
+
+def on_enter(left_frame,right_frame,event, inputtxt):
+    #printInput(root,inputtxt)
+    inp = inputtxt.get("end-1c linestart", "end-1c lineend")
+    if inp != "":
+        activity = Activity(right_frame,inputtxt, 0,0).plot_graph()
+        activities.append(activity)
+        ctk.set_default_color_theme("blue")
+        activity_button = ctk.CTkButton(master=left_frame, text=inp)
+        activity_button.grid(row=len(activities)+10, column=1,sticky="nsew", pady=1)
+        #activity_label = tk.Label(left_frame,text=inp, font = ("Verdana 10 bold", 25),fg = "blue",bg = "yellow")
+        #activity_label.grid(row=len(activities)+10, column=1,sticky="nsew", pady=1)
+    else:
+        pass
+    
 def get_button_width(button, inputtxt):
     add_activity_button_width = button.winfo_width()
     inputtxt.place(x=50+add_activity_button_width+90, y=50)
 
-def add_activity_button_command(root,event,inputtxt):
-    printInput(root,inputtxt)
+def add_activity_button_command(left_frame,right_frame,event,inputtxt):
+    #printInput(root,inputtxt)
+    inp = inputtxt.get("end-1c linestart", "end-1c lineend")
+    if inp != "":
+        activity = Activity(right_frame,inputtxt, 0,0).plot_graph()
+        activities.append(activity)
+        ctk.set_default_color_theme("blue")
+        activity_button = ctk.CTkButton(master=left_frame, text=inp)
+        activity_button.grid(row=len(activities)+10, column=1,sticky="nsew", pady=1)
+        #activity_label = tk.Label(left_frame,text=inp, font = ("Verdana 10 bold", 25),fg = "blue",bg = "yellow")
+        #activity_label.grid(row=len(activities)+10, column=1,sticky="nsew", pady=1)
+    else:
+        pass
     inputtxt.delete(1.0, ctk.END)
 
 class Activity:
-    def __init__(self, root, name, typ, row, column):
+    def __init__(self, root, name, row, column):
         self.root = root
         self.name = name
-        self.typ = typ
         self.row = row
         self.column = column
         self.plot_graph()
+        #self.goal (daily, weekly,monthly)
         
     def plot_graph(self):
         
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ ctk.set_default_color_theme("green")
 
 font_size = 25  
 
+activities = []
 
 def main():
     root = ctk.CTk()
@@ -28,21 +29,24 @@ def main():
 
     
     inputtxt = tk.Text(left_frame, height = 1, width = 20, font=font,bg="lightgray", padx=10, pady=5)
-    inputtxt.grid(row=0, column=0)
-    inputtxt.bind("<Return>",lambda event: fl.on_enter(root,event, inputtxt))
+    inputtxt.grid(row=1, column=2)
+    inputtxt.bind("<Return>",lambda event: fl.on_enter(left_frame,right_frame,event, inputtxt))
 
+    add_activity_button = ctk.CTkButton(master=left_frame, text="Add Activity")
+    add_activity_button.grid(row=2, column=2)
+    add_activity_button.bind("<Button-1>",lambda event: fl.add_activity_button_command(left_frame,right_frame, event, inputtxt))
+
+    for row in range(3,10):
+        tk.Frame(left_frame,width=183, height=20, bg="black").grid(row=row, column=1, sticky="nsew")
     
 
-    add_activity_button = ctk.CTkButton(master=left_frame, text="Add activity")
-    add_activity_button.grid(row=0, column=1)
-    add_activity_button.bind("<Button-1>",lambda event: fl.add_activity_button_command(root, event, inputtxt))
     #add_activity_button.after_idle(lambda: fl.get_button_width(add_activity_button, inputtxt))  # Get button width after it's fully drawn
 
 
-    activity = fl.Activity(right_frame,"name","typ", 0,0).plot_graph()
-    activity = fl.Activity(right_frame,"name","typ", 0,1).plot_graph()
-    activity = fl.Activity(right_frame,"name","typ", 1,0).plot_graph()
-    activity = fl.Activity(right_frame,"name","typ", 1,1).plot_graph()
+    activity = fl.Activity(right_frame,"name", 0,0).plot_graph()
+    activity = fl.Activity(right_frame,"name", 0,1).plot_graph()
+    activity = fl.Activity(right_frame,"name", 1,0).plot_graph()
+    activity = fl.Activity(right_frame,"name", 1,1).plot_graph()
         
     root.mainloop()
 
